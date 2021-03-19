58a861110e6b920aebb6ef75d02a0dc8bed244a5
Phil Scott <philscott.dev@gmail.com>
2021-03-19 10:42:30 -0400

    updated docs

--- stories/Pagination.stories.tsx
+++ stories/Pagination.stories.tsx
@@ -10,9 +10,30 @@ export default {
   argTypes: {},
 } as Meta
 
-const Template: Story<PaginationProps> = () => {
-  const [page, setPage] = useState(1)
-  return <Pagination page={page} pageCount={10} onChangePage={setPage} />
+const Template: Story<PaginationProps> = ({
+  page: defaultPage,
+  pageCount,
+  onChangePage,
+}) => {
+  const [page, setPage] = useState(defaultPage)
+
+  const handleChangePage = (nextPage: number) => {
+    setPage(nextPage)
+    onChangePage(nextPage)
+  }
+
+  return (
+    <Pagination
+      page={page}
+      pageCount={pageCount}
+      onChangePage={handleChangePage}
+    />
+  )
 }
 
 export const Default = Template.bind({})
+Default.args = {
+  page: 1,
+  pageCount: 10,
+  onChangePage: (nextPage) => console.log(nextPage),
+}
